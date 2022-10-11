f7cc82ef402e408f78471042f8bf428ba886fe1d
Caduaaraujoo <caduaaraujoo@gmail.com>
2022-10-11 20:10:09 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+Console.WriteLine("Digite seu Nome:");
+p1.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite seu Sobrenome:");
+p1.Sobrenome = Console.ReadLine();
+
+Console.WriteLine("Dgite sua Idade:");
+p1.Idade = int.Parse(Console.ReadLine());
+
+p1.Apresentar();
